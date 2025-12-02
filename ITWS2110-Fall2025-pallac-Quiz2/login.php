e426ca4ee54027f5fd1643765e36f92b3506154c
Cyrus Palladino <pallac@rpi.edu>
2025-12-01 21:04:48 -0500

    Update login.php

--- ITWS2110-Fall2025-pallac-Quiz2/login.php
+++ ITWS2110-Fall2025-pallac-Quiz2/login.php
@@ -1,4 +1,12 @@
+
+
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+require 'db.php';
+
 session_start();
 require 'db.php'; // mysqli connection file
 
